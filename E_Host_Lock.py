2716240d4bab8d3a744998d072075ad51f0eb241
Yuki <134757820+yuki-4201@users.noreply.github.com>
2025-01-26 01:50:35 +0900

    Update E_Host_Lock.py

--- E_Host_Lock.py
+++ E_Host_Lock.py
@@ -3,17 +3,37 @@ import datetime
 from random import randint
 from realtime import AsyncRealtimeClient
 import A_Setting
-import G_Lock
+import H_Unlock
+import I_Passcode
 
-# イベントを受信するための設定
+#Supabaseの設定
 url = A_Setting.host_url
 key = A_Setting.login_database_Key
 
-# イベント名とチャンネル名
-eventName = "RequestForLocking"
+#ブロードキャストの設定
+eventName = "RequestForUnlocking"
 channelName = "admin"
 
-# イベントを受信したときの処理
+Systemstart = A_Setting.Systemstart
+Systemend = A_Setting.Systemend
+
+#パスコードの設定
+newPassCodeInterval = 20
+passCode1, passCode2 = None, None
+
+#パスコードを生成する
+def randomPassCode():
+    return str(randint(1000, 8999))
+
+#パスコードを変更する(直近2つを保持)
+def changePassCodes():
+    global passCode1, passCode2
+    passCode1 = passCode2
+    passCode2 = randomPassCode()
+    print(f"新しいパスコードが生成されました:{passCode2}")
+    I_Passcode.passcode(passCode1,passCode2)
+
+#ブロードキャストを受信したときの処理
 def handle_broadcast(data):
     dt_now = datetime.datetime.now()
 
@@ -21,37 +41,49 @@ def handle_broadcast(data):
     if "event" in data and data["event"] == eventName:
         code = data["payload"]
         user = data["user"]
+        group = data["group"]
+        print(str(dt_now) +f'\t パスコード受信：{code}')
 
-        #コードが一致しているか確認
-        if code and code == 424242:
-            #sys.stdout = open("_Application_Lock.log", "a")
-            print(str(dt_now) + "施錠命令受信:施錠します" + user)
-            #sys.stdout = sys.__stdout__
+        #パスコードが一致しているか確認
+        if code and code == passCode1 or code == passCode2:
+            dt_now = datetime.datetime.now()
+            nowtime = dt_now.time()
 
-            #施錠処理
-            G_Lock.lock("user")
+            if nowtime >= Systemstart and nowtime <= Systemend:
+                #解錠処理
+                H_Unlock.unlock(user)
+            else:
+                print("時間外労働はお断りします")
+
+        elif group == "teacher":
+            print("特別措置")
+            H_Unlock.unlock(user)
+
         else:
-            #sys.stdout = open("_Application_Lock.log", "a")
-            print(str(dt_now) +"施錠失敗")
-            #sys.stdout = sys.__stdout__
+            print("*** パスコードが一致しません")
 
     else:
-        #sys.stdout = open("_Application_Lock.log", "a")
         print(str(dt_now) +f"*** 不明なイベントを受信しました:{data}")
-        #sys.stdout = sys.__stdout__
+
+#新しいパスコードを生成する
+async def generatePassCodeContinuously():
+    while True:
+        changePassCodes()
+        await asyncio.sleep(newPassCodeInterval)
 
 #クライアントからのメッセージを受信する
-async def listenToBloadcastMessages():
+async def listenToBoadcasMessages():
     client = AsyncRealtimeClient(url, key)
     await client.connect()
     channel = client.channel(channelName)
     await channel.on_broadcast(eventName , handle_broadcast).subscribe()
     await client.listen()
 
-#メイン関数
+#メイン処理
 if __name__ == "__main__":
+    changePassCodes()
     loop = asyncio.get_event_loop()
-    tasks = [listenToBloadcastMessages()]
+    tasks = [generatePassCodeContinuously(), listenToBoadcasMessages()]
     try:
         loop.run_until_complete(asyncio.gather(*tasks))
     finally:
