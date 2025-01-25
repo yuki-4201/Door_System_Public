6a5de3632256e3ebb7fe75d583ef9c36f36fd0e1
Yuki <134757820+yuki-4201@users.noreply.github.com>
2025-01-26 01:48:49 +0900

    Update B_Main.py

--- B_Main.py
+++ B_Main.py
@@ -2,7 +2,7 @@ import datetime
 import subprocess
 from supabase import create_client
 import A_Setting
-import E_Suspicious # type: ignore
+import D_Suspicious # type: ignore
 
 #動体検知システムを作動させない時間帯の設定
 Systemstart = A_Setting.Systemstart
@@ -13,12 +13,8 @@ while True:
     dt_now = datetime.datetime.now()
     nowtime = dt_now.time()
 
-    #時刻の表示
-    print(str(nowtime))
-
     #時間帯によるシステムの切り替え
     if nowtime <= Systemstart and nowtime >= Systemend:
         #動体検知システムを作動
-        proc = subprocess.Popen(E_Suspicious)
+        proc = subprocess.Popen(D_Suspicious)
         proc.communicate
-
